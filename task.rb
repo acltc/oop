64199e32eb15534e17c66863890c7af30bcc9b04
Joshua Monzon <Jmonzon0415@gmail.com>
2016-01-03 01:29:20 -0500

    intial commit

--- task.rb
+++ task.rb
@@ -1,12 +1,25 @@
 class Task
-  attr_reader :name, :complete
+  attr_reader :name, :complete, :priority 
 
-  def initialize(name)
+
+  def initialize(name, priority)
     @name = name
     @complete = false
+    @priority = priority
   end
 
   def complete!
     @complete = true
   end
+
+	def toggle_complete!
+     @complete = false
+  end
+
+  def priority
+   	@priority
+  end
+ 
+
 end
+
